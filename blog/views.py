9cd5af3ca6342e9619dca5272dd3360fbe6bbdab
JimBoonie <mcgough.mason@gmail.com>
2017-12-29 12:43:17 -0500

    Made separate home and blog pages;

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,8 @@ from .models import Post
 
 # Create your views here.
 def home(requests):
+    return render(requests, 'home.html')
+
+def blog(requests):
     posts = Post.objects.all()
-    return render(requests, 'home.html', {'posts': posts})
+    return render(requests, 'blog.html', {'posts': posts})
