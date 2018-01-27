b575058bbee6a6d7173efc83618bb6e591749ab1
JimBoonie <mcgough.mason@gmail.com>
2018-01-27 10:09:43 -0500

    Now handles blog posts that are not found;

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django import template
+from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -10,6 +12,12 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date_posted']
+
+    def get_html(self):
+        try:
+            return render_to_string(self.filename)
+        except template.TemplateDoesNotExist:
+            return render_to_string('not-found.html')
 
 class Comment(models.Model):
     content = models.CharField(max_length=255)
