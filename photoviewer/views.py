fced70175dcb6464e25f7d784a4f2be00518890d
JimBoonie <mcgough.mason@gmail.com>
2018-09-24 22:05:26 -0400

    Load photos to filmstrip based on contents of static folder;

--- photoviewer/views.py
+++ photoviewer/views.py
@@ -1,4 +1,19 @@
+from os import listdir
+from os.path import join, dirname, splitext
 from django.shortcuts import render
 
 def photos(request):
-    return render(request, 'photoviewer/photos.html', {})
+    def _is_img(img_name):
+        valid_exts = ('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff')
+        _, ext = splitext(img_name)
+        return True if ext in valid_exts else False
+
+    static_dir = 'photoviewer/zoom'
+    mydir = join(dirname(__file__), 'static', static_dir)
+    imgs_list = [join('/static', static_dir, i) for i in listdir(mydir) if _is_img(i)]
+
+    return render(
+        request, 
+        'photoviewer/photos.html', 
+        {'imgs_list': imgs_list}
+    )
